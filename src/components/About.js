c4173609855ea5aeaca464d7606365eee98d7a83
Redha Nedjari <ned.r@hotmail.fr>
2021-01-20 21:05:43 +0100

    5.  GlobalStyle && AboutStyle

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,66 @@
 import home1 from "../img/home1.png";
+//Style
+import styled from "styled-components";
+
 const About = () => {
   return (
-    <div>
-      <div className="description">
+    <Aboutblock>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dream</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have.
         </p>
-        <buton>Contact Us</buton>
-      </div>
-      <div className="image">
+        <button>Contact Us</button>
+      </Description>
+      <Image>
         <img src={home1} alt="guy with a camera" />
-      </div>
-    </div>
+      </Image>
+    </Aboutblock>
   );
 };
+
+//Styled components
+const Aboutblock = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding: 5rem;
+  h2 {
+    font-weight: 5rem;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default About;
