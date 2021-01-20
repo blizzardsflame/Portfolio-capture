304fcd3741eb4f1a49f23eba7b3d02a447ed4022
Redha Nedjari <ned.r@hotmail.fr>
2021-01-20 20:26:45 +0100

    4. Faq

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import About from "../components/About";
 import Services from "../components/Services";
+import Faq from "../components/Faq";
 
 const AboutUs = () => {
   return (
     <>
       <About />
       <Services />
+      <Faq />
     </>
   );
 };
