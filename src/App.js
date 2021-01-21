c6ea3747083ae9a736881819a3ce56ece32c1baf
Redha Nedjari <ned.r@hotmail.fr>
2021-01-21 15:53:52 +0100

    8. Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 //GlobalStyle
 import GlobalStyle from "./components/GlobalStyle";
-//Components
+//Pages && Components
 import AboutUs from "./pages/AboutUs";
+import Work from "./pages/Work";
+import ContactUs from "./pages/ContactUs";
 import Nav from "./components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <Work />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
