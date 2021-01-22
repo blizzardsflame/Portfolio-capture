71cb9f3a9206f6071776caeae1de46733619ff85
Redha Nedjari <ned.r@hotmail.fr>
2021-01-22 21:50:55 +0100

    9. ProjectDetail && dynamic data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import Work from "./pages/Work";
 import ContactUs from "./pages/ContactUs";
+import ProjectDetail from "./pages/ProjectDetail";
 import Nav from "./components/Nav";
 //Router
 import { Switch, Route } from "react-router-dom";
@@ -17,8 +18,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <Work />
+        </Route>
+        <Route path="/work/:id">
+          <ProjectDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,9 +1,57 @@
+//Route
+import { Link } from "react-router-dom";
+//Style
+import styled from "styled-components";
+//Images
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+
 const Work = () => {
   return (
-    <div>
-      <h1>Work</h1>
-    </div>
+    <Workblock>
+      <Movie>
+        <h2>The Athelete</h2>
+        <div className="line"></div>
+        <Link to="/work/the-athlete">
+          <img src={athlete} alt="athelete" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link to="/work/the-racer">
+          <img src={theracer} alt="The racer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link to="/work/good-times">
+          <img src={goodtimes} alt="good times" />
+        </Link>
+      </Movie>
+    </Workblock>
   );
 };
+
+const Workblock = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+`;
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 
 export default Work;
