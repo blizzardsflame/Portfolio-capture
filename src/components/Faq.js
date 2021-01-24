344e740d52b0ad1140a762ea8b370545c406b989
Redha Nedjari <ned.r@hotmail.fr>
2021-01-24 16:44:37 +0100

    11. Toggle & Faq animation

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,55 +1,51 @@
 import styled from "styled-components";
 import { Aboutblock } from "../Styles";
+import { AnimateSharedLayout } from "framer-motion";
+import Toggle from "./Toggle";
 const Faq = () => {
   return (
     <FaqBlock>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet consectetur.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Aspernatur, ipsam.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet consectetur.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Aspernatur, ipsam.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet consectetur.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Aspernatur, ipsam.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Products do you offer</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet consectetur.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Aspernatur, ipsam.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Aspernatur, ipsam.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Aspernatur, ipsam.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Different Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Aspernatur, ipsam.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What Products do you offer">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Aspernatur, ipsam.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </FaqBlock>
   );
 };
